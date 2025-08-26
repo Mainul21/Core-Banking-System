d19b7073254b9cf8f37d413c8a92325e73b096d0
Mainul21 <mainul.hossain.chisty@g.bracu.ac.bd>
2025-08-26 16:54:12 +0600

    fix: update background image path to use absolute URL

--- cbs-client/src/App.jsx
+++ cbs-client/src/App.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router";
 const App = () => {
   const navigate = useNavigate(); 
   return (
-    <div className="min-h-screen bg-[url('./bg.jpg')] bg-cover bg-center bg-no-repeat">
+    <div className="min-h-screen bg-[url('/bg.jpg')] bg-cover bg-center bg-no-repeat">
       <div className="w-11/12 mx-auto pt-10 p-5 min-h-1/3 ">
         <img src="./CBS.png" className="w-1/4 mx-auto mb-10" alt="" />
         <h1 className="text-5xl font-bold text-center bg-gradient-to-r from-black to-emerald-500 bg-clip-text text-transparent">
